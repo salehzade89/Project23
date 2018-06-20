ce2913cbbaf4368a894edc6813be41f892d83fa1
Alim <salehzade89@gmail.com>
2018-06-20 15:43:42 +0400

    .

--- Project23/main.cpp
+++ Project23/main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 #include <string>
+#include "Observable.cpp"
 
 int main()
 {
+	Observable observable;
 
+	ObserverA observerA1;
+
+	observable.addObserver(&observerA1);
+
+	observable.setText("Hello");
 
 	system("pause");
 	return 0;
